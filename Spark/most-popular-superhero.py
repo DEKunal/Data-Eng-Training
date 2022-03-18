54296d74789a0ee7869a8fda119240ca9df2a989
kunal Nandwana <kunal.nandwana800@gmail.com>
2022-03-18 13:43:25 +0530

    most-popular-superhero

---
+++ Spark/most-popular-superhero.py
@@ -0,0 +1,32 @@
+import imp
+from mysqlx import ModifyStatement
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as func
+from pyspark.sql.types import StructField,StructType,StringType,IntegerType,FloatType,LongType
+
+spark=SparkSession.builder.appName("Most Populatr Superhero").getOrCreate()
+
+schema=StructType([
+    StructField("id",IntegerType(),True),
+    StructField("name",StringType(),True)
+])
+
+names=spark.read.schema(schema).option("sep"," ").csv("file:///home/user/sparkcourse/Marvel-Names")
+
+
+lines=spark.read.text("file:///home/user/sparkcourse/Marvel-Graph")
+
+
+connections=lines.withColumn("id",func.split(func.col("value")," ")[0])\
+    .withColumn("connections",func.size(func.split(func.col("value")," "))-1)\
+        .groupBy("id").agg(func.sum("connections").alias("connections"))
+
+
+
+mostPopular=connections.sort(func.col("connections").desc()).first()
+
+
+mostPopularName=names.filter(func.col("id")==mostPopular[0]).select("name").first()
+
+print(mostPopularName[0] + " is the most popular superhero with " + str(mostPopular[1]) + " co-appearences.")
+
